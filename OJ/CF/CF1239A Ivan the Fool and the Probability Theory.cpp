04685a24f42614a4345b49e3b2b8070e50199d58
G-hsm <54429611+G-hsm@users.noreply.github.com>
2019-10-21 22:02:25 +0800

    Create CF1239A Ivan the Fool and the Probability Theory.cpp

---
+++ OJ/CF/CF1239A Ivan the Fool and the Probability Theory.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#define file(s) freopen(s".in", "r", stdin), freopen(s".out", "w", stdout)
+
+#define DeBug(x) std::cout << #x << " = " << x << std::endl
+
+typedef long long ll;
+const int MaxN = 1e5 + 10, mod = 1e9 + 7;
+
+namespace IO
+{
+	#define Grt ch = getchar()
+	char buf[1<<15], *fs, *ft;
+	inline char getc() { return ft == fs && (ft = (fs = buf) + fread(buf, 1, 1 << 15, stdin), ft == fs) ? 0 : *fs ++; }
+	template <typename T> inline void read(T &x)
+	{
+		x = 0;
+		T f = 1, Grt;
+		while (!isdigit(ch) && ch ^ '-') Grt;
+		if (ch == '-') f = -1, Grt;
+		while (isdigit(ch)) x = (x << 1) + (x << 3) + (ch ^ 48), Grt;
+		x *= f;
+	}
+
+	template <typename T, typename... Args>
+	inline void read(T &x, Args &...args) { read(x); read(args...); }
+
+	char Out[1<<24], *fe = Out;
+	inline void flush() { fwrite(Out, 1, fe - Out, stdout); fe = Out; }
+	template <typename T> inline void write(T x, char str, bool fl)
+	{
+		if (!x) *fe++ = 48;
+		if (x < 0) *fe++ = '-', x = -x;
+		T sum = 0, ch[20];
+		while (x) ch[++ sum] = x % 10 + 48, x /= 10;
+		while (sum) *fe++ = ch[sum --];
+		if (fl) *fe++ = str;
+	}
+}
+
+using IO::read;
+using IO::write;
+
+template <typename T> inline bool chkMin(T &a, const T &b) { return a > b ? (a = b, true) : false; }
+template <typename T> inline bool chkMax(T &a, const T &b) { return a < b ? (a = b, true) : false; }
+template <typename T> inline T min(T a, T b) { return a < b ? a : b; }
+template <typename T> inline T max(T a, T b) { return a > b ? a : b; }
+
+ll f[MaxN][2];//f[i][0/1] 表示第 i 个格子的颜色为黑白的合法方案数
+int main()
+{
+	int n, m; read(n, m);
+	f[1][0] = f[1][1] = 1;
+	f[2][0] = f[2][1] = 2;
+	for (int i = 3; i <= max(n, m); ++ i)
+		for (int j = 0; j <= 1; ++ j) f[i][j] = (f[i - 1][j ^ 1] % mod + f[i - 2][j ^ 1] % mod) % mod;
+	write((f[n][0] + f[n][1] + f[m][0] + f[m][1] - 2) % mod, '\n', true);
+	IO::flush();
+	return 0;
+}
