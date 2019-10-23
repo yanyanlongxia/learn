bb5c435d90f2fefa91d4b7e1b688f6632bcb7324
G-hsm <54429611+G-hsm@users.noreply.github.com>
2019-10-23 17:10:16 +0800

    Create A Yet Another Dividing into Teams.cpp

---
+++ OJ/CF/1249/A Yet Another Dividing into Teams.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#define file(s) freopen(s".in", "r", stdin), freopen(s".out", "w", stdout)
+
+#define DeBug(x) std::cout << #x << " = " << x << std::endl
+
+const int MaxN = 1e3 + 10;
+
+namespace IO
+{
+	#define G ch = getchar()
+	char buf[1<<15], *fs, *ft;
+	inline char getc() { return ft == fs && (ft = (fs = buf) + fread(buf, 1, 1 << 15, stdin), ft == fs) ? 0 : *fs ++; }
+	template <typename T> inline void read(T &x)
+	{
+		x = 0;
+		T f = 1, G;
+		while (!isdigit(ch) && ch ^ '-') G;
+		if (ch == '-') f = -1, G;
+		while (isdigit(ch)) x = (x << 1) + (x << 3) + (ch ^ 48), G;
+		x *= f;
+	}
+
+	template <typename T, typename... Args>
+	inline void read(T &x, Args &...args) { read(x); read(args...); }
+
+	char Out[1<<24], *fe = Out;
+	inline void flush() { fwrite(Out, 1, fe - Out, stdout); fe = Out; }
+	template <typename T> inline void write(T x, char str, bool fl)
+	{
+		if (!x) *fe++ = 48;
+		if (x < 0) *fe++ = '-', x = -x;
+		T num = 0, ch[20];
+		while (x) ch[++ num] = x % 10 + 48, x /= 10;
+		while (num) *fe++ = ch[num --];
+		if (fl) *fe++ = str;
+	}
+}
+
+using IO::read;
+using IO::write;
+
+template <typename T> inline bool chkMin(T &a, const T &b) { return a > b ? (a = b, true) : false; }
+template <typename T> inline bool chkMax(T &a, const T &b) { return a < b ? (a = b, true) : false; }
+template <typename T> inline T min(T a, T b) { return a < b ? a : b; }
+template <typename T> inline T max(T a, T b) { return a > b ? a : b; }
+
+int a[MaxN];
+int main()//最多只有两队，感性想一下即可知道，然后排序过后，如果有 ai - aj = 1，那么肯定有两队，否则有一队
+{
+	int Q; read(Q);
+	while (Q --)
+	{
+		int n; read(n);
+		for (int i = 1; i <= n; ++ i) read(a[i]);
+		std::sort(a + 1, a + n + 1);
+		int ans = 1;
+		for (int i = 2; i <= n; ++ i)
+			if (a[i] == a[i - 1] + 1) ans = 2;
+		write(ans, '\n', true);
+	}
+	IO::flush();
+	return 0;
+}
