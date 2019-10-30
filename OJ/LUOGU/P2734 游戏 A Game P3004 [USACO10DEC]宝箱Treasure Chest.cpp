4c8408a3dd58c4d35f2737d13dbd86062de3823b
G-hsm <54429611+G-hsm@users.noreply.github.com>
2019-10-30 08:58:21 +0800

    Create P2734 游戏 A Game P3004 [USACO10DEC]宝箱Treasure Chest.cpp

---
+++ OJ/LUOGU/P2734 游戏 A Game P3004 [USACO10DEC]宝箱Treasure Chest.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#define file(s) freopen(s".in", "r", stdin), freopen(s".out", "w", stdout)
+
+#define DeBug(x) std::cout << #x << " = " << x << std::endl
+
+const int MaxN = 5e3 + 10;
+
+namespace IO
+{
+	#define Grt ch = getchar()
+	char buf[1<<15], *fs, *ft;
+	inline char getc() { return ft == fs && (ft = (fs = buf) + fread(buf, 1, 1 << 15, stdin), ft == fs) ? 0 : *fs ++; }
+	template <typename T> inline void read(T &x)
+	{
+		x = 0;
+		T f = 1, Grt;
+		while (!isdigit(ch) && ch ^ '-') Grt;
+		if (ch == '-') f = -1, Grt;
+		while (isdigit(ch)) x = (x << 1) + (x << 3) + (ch ^ 48), Grt;
+		x *= f;
+	}
+
+	template <typename T, typename... Args>
+	inline void read(T &x, Args &...args) { read(x); read(args...); }
+
+	char Out[1<<24], *fe = Out;
+	inline void flush() { fwrite(Out, 1, fe - Out, stdout); fe = Out; }
+	template <typename T> inline void write(T x, char str, bool fl)
+	{
+		if (!x) *fe++ = 48;
+		if (x < 0) *fe++ = '-', x = -x;
+		T num = 0, ch[20];
+		while (x) ch[++ num] = x % 10 + 48, x /= 10;
+		while (num) *fe++ = ch[num --];
+		if (fl) *fe++ = str;
+	}
+}
+
+using IO::read;
+using IO::write;
+
+template <typename T> inline bool chkMin(T &a, const T &b) { return a > b ? (a = b, true) : false; }
+template <typename T> inline bool chkMax(T &a, const T &b) { return a < b ? (a = b, true) : false; }
+template <typename T> inline T min(T a, T b) { return a < b ? a : b; }
+template <typename T> inline T max(T a, T b) { return a > b ? a : b; }
+
+int f[MaxN];//f[i][j] 表示取区间 [i,j] player1 最高得分
+int sum[MaxN];//前缀和
+int main()
+{
+	int n; read(n);
+	for (int i = 1, a; i <= n; ++ i) read(a), sum[i] = sum[i - 1] + a, f[i] = a;
+	for (int len = 2; len <= n; ++ len)//len 为长度,注意从 2 开始
+		for (int l = 1; l + len - 1 <= n; ++ l)//枚举 l
+		{
+            int r = l + len - 1;//计算出 r
+            f[l] = sum[r] - sum[l - 1] - min(f[l], f[l + 1]);
+        }
+	write(f[1], ' ', true);
+	write(sum[n] - f[1], '\n', true);
+	IO::flush();
+	return 0;
+}
