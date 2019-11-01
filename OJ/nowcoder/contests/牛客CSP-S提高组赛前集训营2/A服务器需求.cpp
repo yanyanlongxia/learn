0adab2c2d7c709dbf377cbfd848d19f84d0ea070
G-hsm <54429611+G-hsm@users.noreply.github.com>
2019-11-01 22:17:06 +0800

    Update A服务器需求.cpp

--- OJ/nowcoder/contests/牛客CSP-S提高组赛前集训营2/A服务器需求.cpp
+++ OJ/nowcoder/contests/牛客CSP-S提高组赛前集训营2/A服务器需求.cpp
@@ -48,8 +48,8 @@ template <typename T> inline T max(T a, T b) { return a > b ? a : b; }
 
 int a[MaxN];
 std::multiset<int> s;
-int main()
-{
+int main()//现在想想，当时真是傻，这么简单的结论，要么是最大的 a[i]，要么是平均值，这很显然，所以加上的 m 天可以不连续就是必要的，如果没有这句话，才会牵扯更多的东西
+{//但是毕竟不能每次修改一个东西就 sort 吧，所以就要借助一个可以自动排序的 stl ，比如说 priority_queue，multiset之类的，此题完结
 	int n, m, q; read(n, m, q);
 	ll sum = 0;
 	for (int i = 1; i <= n; ++ i) read(a[i]), sum += a[i], s.insert(a[i]);
