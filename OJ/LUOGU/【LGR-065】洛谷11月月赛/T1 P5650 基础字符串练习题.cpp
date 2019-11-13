a1ff6eb6788cc48b94ac36b88f998eaf76729259
G-hsm <54429611+G-hsm@users.noreply.github.com>
2019-11-13 22:55:00 +0800

    Create T1 P5650 基础字符串练习题.cpp

---
+++ OJ/LUOGU/【LGR-065】洛谷11月月赛/T1 P5650 基础字符串练习题.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#define file(s) freopen(s ".in", "r", stdin), freopen(s ".out", "w", stdout)
+
+#define DeBug(x) std::cout << #x << " = " << x << std::endl
+
+typedef long long ll;
+const int MaxN = 2e5 + 10;
+
+namespace IO
+{
+	#define Grt ch = getchar()
+	char buf[1<<15], *fs, *ft;
+	inline char getc() { return ft == fs && (ft = (fs = buf) + fread(buf, 1, 1 << 15, stdin), ft == fs) ? 0 : *fs ++; }
+	template <typename T> inline void read(T &x)
+	{
+		x = 0;
+		T f = 1, Grt;
+		while (!isdigit(ch) && ch ^ '-') Grt;
+		if (ch == '-') f = -1, Grt;
+		while (isdigit(ch)) x = (x << 1) + (x << 3) + (ch ^ 48), Grt;
+		x *= f;
+	}
+
+	template <typename T, typename... Args>
+	inline void read(T &x, Args &...args) { read(x); read(args...); }
+
+	char Out[1<<24], *fe = Out;
+	inline void flush() { fwrite(Out, 1, fe - Out, stdout); fe = Out; }
+	template <typename T> inline void write(T x, char str, bool fl)
+	{
+		if (!x) *fe++ = 48;
+		if (x < 0) *fe++ = '-', x = -x;
+		T num = 0, ch[20];
+		while (x) ch[++ num] = x % 10 + 48, x /= 10;
+		while (num) *fe++ = ch[num --];
+		if (fl) *fe++ = str;
+	}
+}
+
+using IO::read;
+using IO::write;
+
+template <typename T> inline bool chkMin(T &a, const T &b) { return a > b ? (a = b, true) : false; }
+template <typename T> inline bool chkMax(T &a, const T &b) { return a < b ? (a = b, true) : false; }
+template <typename T> inline T min(T a, T b) { return a < b ? a : b; }
+template <typename T> inline T max(T a, T b) { return a > b ? a : b; }
+
+char s[MaxN];
+int main()
+{
+	scanf("%s", s + 1);
+	int n = strlen(s + 1), ans = 0, sum = 0, flag = 0;
+	for (int i = 1; i <= n; ++ i)
+	{
+		if (s[i] == '0') ++ sum, flag = 1;//把 0 当做 1
+		else sum > 0 ? -- sum : 0;//把 1 当做 -1，避免全 1，所以只有在有 0 的时候才减 1
+		chkMax(ans, sum);//每个子串取个 max
+	}
+	if (!flag) -- ans;//全 1 要输出 -1
+	write(ans, '\n', true);
+	IO::flush();
+	return 0;
+}
