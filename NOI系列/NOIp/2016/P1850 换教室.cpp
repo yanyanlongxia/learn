06807a9a316b57339818f94e01ff3c98ab7e03cc
G-hsm <54429611+G-hsm@users.noreply.github.com>
2019-11-15 09:14:30 +0800

    Update P1850 换教室.cpp

--- NOI系列/NOIp/2016/P1850 换教室.cpp
+++ NOI系列/NOIp/2016/P1850 换教室.cpp
@@ -61,20 +61,20 @@ int main()
 		for (int i = 1; i <= v; ++ i)
 			for (int j = 1; j <= v; ++ j) chkMin(d[i][j], d[i][k] + d[k][j]);//Floyed 处理最短路
 	for (int i = 1; i <= v; ++ i) d[i][i] = d[i][0] = d[0][i] = 0;
-    for (int i = 0; i <= n; ++ i)
+	for (int i = 0; i <= n; ++ i)
         for (int j = 0; j <= m; ++ j) f[i][j][0] = f[i][j][1] = inf;
 
 	f[1][0][0] = f[1][1][1] = 0;
-    for (int i = 2; i <= n; ++ i)
+	for (int i = 2; i <= n; ++ i)
 	{
-        f[i][0][0] = f[i - 1][0][0] + d[c[i - 1][0]][c[i][0]];
-        for (int j = 1; j <= min(i, m); ++ j)
+		f[i][0][0] = f[i - 1][0][0] + d[c[i - 1][0]][c[i][0]];
+		for (int j = 1; j <= min(i, m); ++ j)
 		{
-            int C1 = c[i - 1][0], C2 = c[i - 1][1], C3 = c[i][0], C4 = c[i][1];//分类讨论，巨长
-            chkMin(f[i][j][0], min(f[i - 1][j][0] + d[C1][C3], f[i - 1][j][1] + d[C1][C3] * (1 - K[i - 1]) + d[C2][C3] * K[i - 1]));
-            chkMin(f[i][j][1], min(f[i - 1][j - 1][0] + d[C1][C3] * (1 - K[i]) + d[C1][C4] * K[i], f[i - 1][j - 1][1] + d[C2][C4] * K[i] * K[i - 1] + d[C2][C3] * K[i - 1] * (1 - K[i]) + d[C1][C4] * (1 - K[i - 1]) * K[i] + d[C1][C3] * (1 - K[i - 1]) * (1 - K[i])));
+			int C1 = c[i - 1][0], C2 = c[i - 1][1], C3 = c[i][0], C4 = c[i][1];//分类讨论，巨长
+			chkMin(f[i][j][0], min(f[i - 1][j][0] + d[C1][C3], f[i - 1][j][1] + d[C1][C3] * (1 - K[i - 1]) + d[C2][C3] * K[i - 1]));
+			chkMin(f[i][j][1], min(f[i - 1][j - 1][0] + d[C1][C3] * (1 - K[i]) + d[C1][C4] * K[i], f[i - 1][j - 1][1] + d[C2][C4] * K[i] * K[i - 1] + d[C2][C3] * K[i - 1] * (1 - K[i]) + d[C1][C4] * (1 - K[i - 1]) * K[i] + d[C1][C3] * (1 - K[i - 1]) * (1 - K[i])));
 		}
-    }
+	}
 	double ans = inf;
 	for (int i = 0; i <= m; ++ i) chkMin(ans, min(f[n][i][0], f[n][i][1]));
 	printf("%.2f\n", ans);
