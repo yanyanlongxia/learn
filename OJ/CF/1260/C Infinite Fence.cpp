6675bf86f5d403f6c6f3f2c6733c21fddfdb13af
G-hsm <54429611+G-hsm@users.noreply.github.com>
2019-11-29 15:14:52 +0800

    Create C Infinite Fence.cpp

---
+++ OJ/CF/1260/C Infinite Fence.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#define file(s) freopen(s ".in", "r", stdin), freopen(s ".out", "w", stdout)
+
+#define DeBug(x) std::cout << #x << " = " << x << std::endl
+
+typedef long long ll;
+
+namespace IO
+{
+	#define Grt ch = getchar()
+	char buf[1 << 15], *fs, *ft;
+	inline char getc() { return ft == fs && (ft = (fs = buf) + fread(buf, 1, 1 << 15, stdin), ft == fs) ? 0 : *fs ++; }
+	template <typename T> inline void read(T &x)
+	{
+		x = 0;
+		T f = 1, Grt;
+		while (!isdigit(ch) && ch ^ '-') Grt;
+		if (ch == '-') f = -1, Grt;
+		while (isdigit(ch)) x = (x << 1) + (x << 3) + (ch ^ 48), Grt;
+		x *= f;
+	}
+
+	template <typename T, typename... Args>
+	inline void read(T &x, Args &...args) { read(x); read(args...); }
+
+	char Out[1 << 24], *fe = Out;
+	inline void flush() { fwrite(Out, 1, fe - Out, stdout); fe = Out; }
+	template <typename T> inline void write(T x, char str, bool fl)
+	{
+		if (!x) *fe++ = 48;
+		if (x < 0) *fe++ = '-', x = -x;
+		T num = 0, ch[20];
+		while (x) ch[++ num] = x % 10 + 48, x /= 10;
+		while (num) *fe++ = ch[num --];
+		if (fl) *fe++ = str;
+	}
+}
+
+using IO::read;
+using IO::write;
+
+template <typename T> inline bool chkMin(T &a, const T &b) { return a > b ? (a = b, true) : false; }
+template <typename T> inline bool chkMax(T &a, const T &b) { return a < b ? (a = b, true) : false; }
+template <typename T> inline T min(T a, T b) { return a < b ? a : b; }
+template <typename T> inline T max(T a, T b) { return a > b ? a : b; }
+
+template <typename T> inline T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+
+int main()
+{
+	int T; read(T);
+	while (T --)
+	{
+		int a, b, k; read(a, b, k);
+		int g = gcd(a, b);
+		a /= g, b /= g;
+		if (a > b) std::swap(a, b);
+		puts((a - 1 + b - 1) / a >= k ? "REBEL" : "OBEY");
+	}
+	return 0;
+}
